dac2732ab0364908b2438791994f8a72255bbc3c
Juan Pablo Quiroga <jquiroga1991@gmail.com>
2017-04-13 19:03:36 -0300

    comit antes de deploy

--- client/imports/ui/pages/public/zanelli1319.jsx
+++ client/imports/ui/pages/public/zanelli1319.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import BigTitle from '../../components/big-title'
 import Text from '../../components/text'
 import YellowButton from '../../components/yellowbutton'
+//import SimpleMap from '../components/googlemaps'
+import { AutoRotatingCarousel, Slide } from 'material-auto-rotating-carousel'
+import { green400, green600, blue400, blue600, red400, red600 } from 'material-ui/styles/colors'
 
 export default class Zanelli1319 extends React.Component {
   render () {
@@ -42,7 +45,44 @@ Oficina de 40 m2, con 7 estaciones, mesa para reuniones y acceso directo al pati
         </div>
         <BigTitle title='HABITANTES EN ZANELLI' small />
         <img src='zanelli1.png' style={{display: 'block', margin: '0 auto'}} />
+        {/*<div>
+
+     <AutoRotatingCarousel
+       label="Get started"
+       open
+     >
+       <Slide
+         media={<img src="http://www.icons101.com/icon_png/size_256/id_79394/youtube.png" />}
+         mediaBackgroundStyle={{ backgroundColor: red400 }}
+         contentStyle={{ backgroundColor: red600 }}
+         title="This is a very cool feature"
+         subtitle="Just using this will blow your mind."
+       />
+       <Slide
+         media={<img src="http://www.icons101.com/icon_png/size_256/id_80975/GoogleInbox.png" />}
+         mediaBackgroundStyle={{ backgroundColor: blue400 }}
+         contentStyle={{ backgroundColor: blue600 }}
+         title="Ever wanted to be popular?"
+         subtitle="Well just mix two colors and your are good to go!"
+       />
+       <Slide
+         media={<img src="http://www.icons101.com/icon_png/size_256/id_76704/Google_Settings.png" />}
+         mediaBackgroundStyle={{ backgroundColor: green400 }}
+         contentStyle={{ backgroundColor: green600 }}
+         title="May the force be with you"
+         subtitle="The Force is a metaphysical and ubiquitous power in the Star Wars universe."
+       />
+     </AutoRotatingCarousel>
+   </div>*/}
+   <div class="carousel carousel-slider">
+<a class="carousel-item" href="#one!"><img src="http://lorempixel.com/800/400/food" /></a>
+<a class="carousel-item" href="#two!"><img src="http://lorempixel.com/800/400/sports" /></a>
+<a class="carousel-item" href="#three!"><img src="http://lorempixel.com/800/400/nature" /></a>
+<a class="carousel-item" href="#four!"><img src="http://lorempixel.com/800/400/fashion" /></a>
+</div>
       </div>
+
+
     )
   }
 }
