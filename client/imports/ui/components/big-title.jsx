2fa32d93253382eb8fba91aebab69e4934fa7d2a
Juan Pablo Quiroga <jquiroga1991@gmail.com>
2017-05-05 18:35:11 -0300

    TODO ARREGLADO SOLO FALTAN LOGOS

--- client/imports/ui/components/big-title.jsx
+++ client/imports/ui/components/big-title.jsx
@@ -11,13 +11,13 @@ export default class BigTitle extends React.Component {
   render () {
     if (this.props.small) {
       return (
-        <h2 style={_.extend({letterSpacing: '2px', fontWeight: 600, fontFamily: 'Raleway', textAlign: 'center', width: '100%', display: 'block', fontSize: 20}, this.props.style)}>
+        <h2 style={_.extend({letterSpacing: '2px', fontWeight: 600, fontFamily: 'Raleway', width: '100%', display: 'block', fontSize: 20}, this.props.style)}>
           {this.props.title}
         </h2>
       )
     }
     return (
-      <h2 className='bigtitle' style={_.extend({letterSpacing: '2px', fontWeight: 600, fontFamily: 'Raleway', textAlign: 'center', width: '100%', display: 'block', fontSize: 42, margin: '40px auto'}, this.props.style)}>
+      <h2 className='bigtitle' style={_.extend({letterSpacing: '2px', fontWeight: 600, fontFamily: 'Raleway', width: '100%', display: 'block', fontSize: 42, margin: '40px auto'}, this.props.style)}>
         {this.props.title}
       </h2>
     )
